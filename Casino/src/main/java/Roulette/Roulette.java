71f96e03406a3e9c8f7cca747a9a605d2a72c45e
LukasGilgen <LukasJan.Gilgen@stud.bbbaden.ch>
2019-04-04 13:01:07 +0200

    Signed-off-by: LukasGilgen <LukasJan.Gilgen@stud.bbbaden.ch>

--- Casino/src/main/java/Roulette/Roulette.java
+++ Casino/src/main/java/Roulette/Roulette.java
@@ -8,6 +8,12 @@ package Roulette;
 
 import com.team1.casino.MainApp;
 import com.team1.casino.Spiel;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -24,5 +30,17 @@ public class Roulette extends Spiel {
     public void startGame() {
         System.out.println("Roulette is working");
         Stage stageRoulette = super.getMainApp().getStage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/RouletteFXML.fxml"));
+        Parent root = null;
+        try {
+            root = (Parent) loader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(Roulette.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        stageRoulette.setTitle("Roulette Wheel");
+        stageRoulette.setScene(scene);
+        stageRoulette.show();
     }
 }

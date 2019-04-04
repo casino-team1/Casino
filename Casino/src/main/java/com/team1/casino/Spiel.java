c30002a6795b6b96a2310a7ab25b1ac4a7aa7e68
Nick Flückiger <nick.flueckiger@outlook.de>
2019-04-04 09:14:33 +0200

    First setup

---
+++ Casino/src/main/java/com/team1/casino/Spiel.java
@@ -0,0 +1,17 @@
+/*
+ * This peace of Software has been written by Nick Flückiger
+ * You are free to use and modifiy this software to your needs
+ * For information and contact with the developer please write to
+ * Mail: nick.flueckiger@outlook.de
+ */
+package com.team1.casino;
+
+/**
+ *
+ * @author Nick Flückiger
+ */
+public abstract class Spiel {
+
+    private MainApp mainApplication;
+
+}

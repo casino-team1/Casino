e25e226227c2c982cf1fa93a608115f8f2a93672
LukasGilgen <LukasJan.Gilgen@stud.bbbaden.ch>
2019-04-04 15:18:13 +0200

    Updated "importantCode.txt" und teil von meinem Code Signed-off-by: LukasGilgen <LukasJan.Gilgen@stud.bbbaden.ch>

--- Casino/src/main/java/Roulette/MainMenuRouletteFXMLController.java
+++ Casino/src/main/java/Roulette/MainMenuRouletteFXMLController.java
@@ -7,7 +7,9 @@ package Roulette;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -15,6 +17,9 @@ import javafx.fxml.Initializable;
  * @author Lukas Gilgen Schule
  */
 public class MainMenuRouletteFXMLController implements Initializable {
+
+    @FXML
+    private Button rouletteStartButton;
 
     /**
      * Initializes the controller class.

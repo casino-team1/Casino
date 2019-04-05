10cfc218e852b8763f428d07fbc50c8a14001813
Nick Flückiger <nick.flueckiger@outlook.de>
2019-04-05 11:57:48 +0200

    Add stats

--- Database/Database.sql
+++ Database/Database.sql
@@ -13,18 +13,26 @@ CREATE TABLE Balance(
     PRIMARY KEY (BalanceID)
 );
 
+CREATE TABLE Statistic(
+    statisticID INTEGER auto_increment,
+    gamePlayed varchar(255),
+    result varchar(255)
+);
+
 CREATE TABLE Player(
     playerID INTEGER auto_increment,
     username varchar(255),
     password varchar(255),
     balanceID integer,
+    statisticID integer,
     PRIMARY KEY (playerID),
-    FOREIGN KEY (balanceID) REFERENCES Balance(balanceID)
+    FOREIGN KEY (balanceID) REFERENCES Balance(balanceID),
+    FOREIGN KEY (statisticID) REFERENCES Statistic(statisticID)
 );
 
-
+INSERT INTO Statistic(gamePlayed,result) VALUES("Baccara","Won");
 INSERT INTO Balance(Balance,LastUpdated) VALUES(1000.0,CURDATE());
 -- Username == "Muster" -- Password: "1234"
 -- Hash $2a$10$VeufAquh14j2F7GVuQa/.uHT0TGfg3yejOdPPvKN0RMjR6IL9ibeK
 
-INSERT INTO Player(username,password,balanceID) VALUES("Muster","$2a$10$VeufAquh14j2F7GVuQa/.uHT0TGfg3yejOdPPvKN0RMjR6IL9ibeK",1);
+INSERT INTO Player(username,password,statisticID,balanceID) VALUES("Muster","$2a$10$VeufAquh14j2F7GVuQa/.uHT0TGfg3yejOdPPvKN0RMjR6IL9ibeK",1,1);

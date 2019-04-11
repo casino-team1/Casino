ecde2bbc82ab6d6b20381dee5052c2063943ac1b
albionspahija <32865170+albionspahija@users.noreply.github.com>
2019-04-11 16:50:52 +0200

    Add Ass-Wert veränderbar
    Signed-off-by: albionspahija <32865170+albionspahija@users.noreply.github.com>

--- Casino/src/main/java/Blackjack/BlackJackDealerModel.java
+++ Casino/src/main/java/Blackjack/BlackJackDealerModel.java
@@ -17,18 +17,57 @@ import javafx.scene.control.Label;
  */
 public class BlackJackDealerModel {
 
+    private int kartenWertDealer = 0;
+    private int karteZweiWert = 0;
+
+    Karten k;
+    private HashMap<String, Integer> karten;
+    private ArrayList<String> kartenSymbole;
+    private ArrayList<String> kartenDealer;
+    private String zufallskarte;
+
     private boolean gewonnen = false;
 
-    private Random r = new Random();
-    private int zufallszahl = 0;
-    private String zufallskarte = "";
+    public void hit(HashMap<String, Integer> karten, ArrayList<String> kartenSymbole, ArrayList<String> kartenDealer, Label labelKartenDealer) {
+        
+        //
+        int zufallszahl = 0;
+        Random r = new Random();
+        
+        //Erste Karte an Dealer verteilen
+        zufallszahl = r.nextInt(k.getAnzahlKartenInKartenSymbole());
+        zufallskarte = kartenSymbole.get(zufallszahl);
 
-    public void hit() {
+        if (zufallskarte.contains("10") || zufallskarte.contains("J") || zufallskarte.contains("Q") || zufallskarte.contains("K")) {
+            kartenWertDealer += 10;
+        } else if (zufallskarte.contains("A")) {
+            kartenWertDealer += 11;
+        } else {
+            kartenWertDealer += karten.get(zufallskarte);
+        }
+        kartenDealer.add(zufallskarte);
+        karten.remove(zufallskarte);
+        kartenSymbole.remove(zufallskarte);
+        k.subAnzahlKartenImKartenDeck();
+        k.subAnzahlKartenInKartenSymbole();
+        labelKartenDealer.setText(zufallskarte + " + ?");
 
-    }
+        //Zweite unbekannte Karte an Dealer verteilen
+        zufallszahl = r.nextInt(k.getAnzahlKartenInKartenSymbole());
+        zufallskarte = kartenSymbole.get(zufallszahl);
 
-    public void austeilen() {
-
+        if (zufallskarte.contains("10") || zufallskarte.contains("J") || zufallskarte.contains("Q") || zufallskarte.contains("K")) {
+            karteZweiWert += 10;
+        } else if (zufallskarte.contains("A")) {
+            karteZweiWert += 11;
+        } else {
+            karteZweiWert += karten.get(zufallskarte);
+        }
+        kartenDealer.add(zufallskarte);
+        karten.remove(zufallskarte);
+        kartenSymbole.remove(zufallskarte);
+        k.subAnzahlKartenImKartenDeck();
+        k.subAnzahlKartenInKartenSymbole();
     }
 
     public void setGewonnen(boolean g) {
@@ -38,4 +77,37 @@ public class BlackJackDealerModel {
     public boolean hasGewonnen() {
         return gewonnen;
     }
+
+    public int getKartenWertDealer() {
+        return kartenWertDealer;
+    }
+
+    public void setKartenWertDealer(int kartenWertDealer) {
+        this.kartenWertDealer = kartenWertDealer;
+    }
+
+    public String getZufallskarte() {
+        return zufallskarte;
+    }
+
+    public void setZufallskarte(String zufallskarte) {
+        this.zufallskarte = zufallskarte;
+    }
+
+    public int getKarteZweiWert() {
+        return karteZweiWert;
+    }
+
+    public void setKarteZweiWert(int karteZweiDealer) {
+        this.karteZweiWert = karteZweiDealer;
+    }
+
+    public ArrayList<String> getKartenDealer() {
+        return kartenDealer;
+    }
+
+    public void setKartenDealer(ArrayList<String> kartenDealer) {
+        this.kartenDealer = kartenDealer;
+    }
+
 }

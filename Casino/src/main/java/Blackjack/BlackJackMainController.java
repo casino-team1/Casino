d9f72955fd8ce6186545077bffe584228246119c
ErikHess <erikmaximilian.hess>
2019-04-29 13:24:04 +0200

    added balance labels Signed-off-by: ErikHess <erikmaximilian.hess>

--- Casino/src/main/java/Blackjack/BlackJackMainController.java
+++ Casino/src/main/java/Blackjack/BlackJackMainController.java
@@ -8,6 +8,7 @@ package Blackjack;
 import com.team1.casino.Controller.LoginController;
 import com.team1.casino.MainApp;
 import com.team1.casino.Model.CasinoLoginModel;
+import com.team1.casino.User.Util.UserCentral;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,6 +21,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -31,6 +33,8 @@ import javafx.stage.Stage;
 public class BlackJackMainController implements Initializable {
 
     private MainApp main;
+    @FXML
+    private Label balanceLabel;
 
     public void setMain(MainApp main) {
         this.main = main;
@@ -49,6 +53,7 @@ public class BlackJackMainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        balanceLabel.setText("Konto: " + UserCentral.getInstance().getUser().getCurrentBalance() + "$");
     }
 
     @FXML
